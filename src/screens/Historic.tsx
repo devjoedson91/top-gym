020eb0a65c8ce670c72e4b7f645c09e684be88a2
Joedson Ferreira <95484503+devjoedson91@users.noreply.github.com>
2023-04-27 15:02:44 -0300

    Finishing training page

--- src/screens/Historic.tsx
+++ src/screens/Historic.tsx
@@ -1,10 +1,99 @@
 import { useLayoutEffect, useState } from "react";
-import { View, Text, SafeAreaView, ScrollView, TouchableOpacity } from "react-native";
+import { View, Text, SafeAreaView, ScrollView, TouchableOpacity, Pressable } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { CheckBox } from "../components/CheckBox";
+import { Barbell, Repeat, Trash } from "phosphor-react-native";
+import colors from "tailwindcss/colors";
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+
+const trainnings = [
+    {
+        id: 1,
+        name: "Costas",
+        exercises: [
+            {
+                id: 1,
+                name: "Puxada frontal",
+                training: [
+                    {
+                        id: 1,
+                        week_day: 1,
+                        amount_series: 4,
+                        amount_repeat: 12
+                    }
+                ]
+            },
+            {
+                id: 2,
+                name: "Remada curvada",
+                training: [
+                    {
+                        id: 1,
+                        week_day: 1,
+                        amount_series: 4,
+                        amount_repeat: 12
+                    }
+                ]
+            },
+            {
+                id: 3,
+                name: "Remada unilateral",
+                training: [
+                    {
+                        id: 1,
+                        week_day: 1,
+                        amount_series: 4,
+                        amount_repeat: 12
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: 2,
+        name: "Bíceps",
+        exercises: [
+            {
+                id: 4,
+                name: "Rosca direta",
+                training: [
+                    {
+                        id: 1,
+                        week_day: 1,
+                        amount_series: 4,
+                        amount_repeat: 12
+                    }
+                ]
+            },
+            {
+                id: 5,
+                name: "Rosca martelo",
+                training: [
+                    {
+                        id: 1,
+                        week_day: 1,
+                        amount_series: 4,
+                        amount_repeat: 12
+                    }
+                ]
+            },
+            {
+                id: 6,
+                name: "Alternados",
+                training: [
+                    {
+                        id: 1,
+                        week_day: 1,
+                        amount_series: 4,
+                        amount_repeat: 12
+                    }
+                ]
+            }
+        ]
+    }      
+];
 
 export function Historic() {
 
@@ -59,61 +148,52 @@ export function Historic() {
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{ paddingBottom: 100 }}
             >
-                <View className="mb-3">
-                    <Text className="text-white font-medium text-base mb-3">Costas</Text>
-                    <TouchableOpacity 
-                        className="bg-gray_500 mb-3 w-full h-[120px] p-3 rounded-lg flex flex-row items-center justify-between"
-                    >
-                        <View>
-                            <Text className="text-white font-medium text-lg">Remada frontal</Text>
-                            <View className="flex-row">
+                {
+                    trainnings.map((trainning, i) => (
 
-                            </View>
+                        <View className="mb-3" key={`${i}-${trainning.name}`}>
+                            <Text className="text-white font-medium text-base mb-3">
+                                {trainning.name}
+                            </Text>
+                            {
+                                trainning.exercises.map((exercise, i) => (
+                                    <TouchableOpacity 
+                                        className="bg-gray_500 mb-3 w-full h-[120px] p-3 rounded-lg flex flex-row items-center justify-between" 
+                                        key={`${i}-${exercise.name}`}
+                                    >
+                                        <View>
+                                            <Text className="text-white font-medium text-lg mb-4">
+                                                {exercise.name}
+                                            </Text>
+                                            <View className="flex flex-row gap-2 items-center">
+                                                <Barbell size={32} color="#00875F" />
+                                                <Text className="text-white font-medium">
+                                                    {
+                                                        exercise.training.map(item => `${item.amount_series} séries`)
+                                                    }    
+                                                </Text>
+                                                <Repeat size={32} color="#00875F" />
+                                                <Text className="text-white font-medium">
+                                                    {
+                                                        exercise.training.map(item => `${item.amount_repeat} repetições`)
+                                                    }  
+                                                </Text>
+                                            </View>
+                                        </View>
+                                        <View className="flex items-center gap-2">
+                                            <CheckBox checked={true} />
+                                            <Pressable>
+                                                <Trash size={38} color={colors.red['500']} />
+                                            </Pressable>
+                                        </View>
+                                    </TouchableOpacity>
+                                ))
+                            }
                         </View>
-                        <CheckBox checked={true} />
-                    </TouchableOpacity>
-                    <TouchableOpacity 
-                        className="bg-gray_500 mb-3 w-full h-[120px] p-3 rounded-lg flex flex-row items-center justify-between"
-                    >
-                        <View>
-                            <Text className="text-white font-medium text-lg">Remada frontal</Text>
-                            <View className="flex-row">
 
-                            </View>
-                        </View>
-                        <CheckBox checked={true} />
-                    </TouchableOpacity>
-                </View>
-
-                <View className="mb-3">
-                    <Text className="text-white font-medium text-base mb-3">Biceps</Text>
-                    <TouchableOpacity 
-                        className="bg-gray_500 mb-3 w-full h-[120px] p-3 rounded-lg flex flex-row items-center justify-between"
-                    >
-                        <View>
-                            <Text className="text-white font-medium text-lg">Remada frontal</Text>
-                            <View className="flex-row">
-
-                            </View>
-                        </View>
-                        <CheckBox checked={true} />
-                    </TouchableOpacity>
-                </View>
-
-                <View className="mb-3">
-                    <Text className="text-white font-medium text-base mb-3">Trapézio</Text>
-                    <TouchableOpacity 
-                        className="bg-gray_500 mb-3 w-full h-[120px] p-3 rounded-lg flex flex-row items-center justify-between"
-                    >
-                        <View>
-                            <Text className="text-white font-medium text-lg">Remada frontal</Text>
-                            <View className="flex-row">
-
-                            </View>
-                        </View>
-                        <CheckBox checked={true} />
-                    </TouchableOpacity>
-                </View>
+                    ))
+                }
+            
             </ScrollView>
         </SafeAreaView>
     );
